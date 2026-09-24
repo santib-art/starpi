1b2e152015811fb338f18718add05b599a570981
Santisrb <santiago.rojas@uniagustiniana.edu.co>
2026-09-24 15:12:52 +0000

    was

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Route, Routes, Navigate,Link } from 'react-router-dom';
+
 import Inicio from './components/inicio'
 import Coleccion from './components/coleccion'
 import Favoritos from './components/favoritos'
@@ -11,12 +12,26 @@ function App() {
 
   return (
     <>
-    <Inicio/>
-    <Coleccion/>
-    <Favoritos/>
-    <Info/>
-    <Usuario/>
-    <Starwars/>
+    <Router>
+
+        <nav className='c-menu'>
+          <Link to="/" >Inicio</Link>
+          <Link to="/Coleccion" >Coleccion</Link>
+          <Link to="/Favoritos" >Favoritos</Link>
+          <Link to="/Info" >Info</Link>
+          <Link to="/Usuario" >Usuario</Link>
+
+        </nav>
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path='/Coleccion' element={<Coleccion />} />
+        <Route path='/Favoritos' element={<Favoritos />} />
+        <Route path='/Info' element={<Info />} />
+        <Route path='/Usuario' element={<Usuario />} />
+        <Route path='/Starwars/:name' element={<Starwars />} />
+      </Routes>
+    </Router>
+ 
     </>
   )
 }
--- src/components/starwars/index.jsx
+++ src/components/starwars/index.jsx
@@ -1,10 +1,48 @@
-import { useState } from 'react'
+import { useParams } from "react-router-dom"; 
+import { useState, useEffect } from 'react'
+
 import "./style.css"
 
 function Starwars() {
+    const { name } = useParams();
+    const [datapoke, setDatapoke] = useState([]);
+
+    useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(response => response.json())
+      .then(responseData => setDatapoke(responseData))
+      .catch(error => console.error("Error:", error));
+    }, [name]); 
+    console.log(datapoke)
+
+     if (!datapoke || !datapoke.id) return <p>Cargando...</p>;
     return (
         <>
-            <h1>fakeapi</h1>
+           {datapoke.id}
+           {datapoke.name}
+           {datapoke.data_experience}
+           {datapoke.abilities[0].ability.name}
+           <div>
+        <p>{datapoke.id}</p>
+        <h1>{datapoke.name}</h1>
+        <img 
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${datapoke.id}.png`} 
+            alt={datapoke.name} 
+            width="200"
+        />
+
+        <p>{datapoke.id}</p>
+        <p>Altura: {datapoke.height/ 10} m / Peso: {datapoke.weight/ 10} kg</p>
+
+        {datapoke.types && (
+          <p>Tipo(s): {datapoke.types.map(t => t.type.name).join(', ')}</p>
+        )}
+        <p>hp: {datapoke.stats[0].base_stat}</p>
+        <p>Velocidad: {datapoke.stats[5].base_stat}</p>
+        <p>Ataque: {datapoke.stats[1].base_stat} Defensa: {datapoke.stats[2].base_stat}</p>
+        <p>Ataque Especial: {datapoke.stats[3].base_stat} Defensa Especial: {datapoke.stats[4].base_stat}</p>
+
+        </div>
         </>
     )
 }
